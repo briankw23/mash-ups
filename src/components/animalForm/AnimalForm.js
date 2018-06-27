96478d81729cb6624da7b384c3278a4300093027
Brian Wright <briankw23@gmail.com>
2018-06-27 18:53:38 -0500

    components are built

---
+++ src/components/animalForm/AnimalForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './AnimalForm.scss';
+
+class AnimalForm extends React.Component {
+  render () {
+    return (
+      <div className='AnimalForm'>
+        <h2>Animal Form</h2>
+      </div>
+    );
+  }
+};
+
+export default AnimalForm;
